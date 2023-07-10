30202eab518ae00eff57869ba982c904eeff986c
Poojitha-Thulluru <thulluru.poojitha@gmail.com>
2023-07-10 22:20:18 +0530

    Function implemented using Try-Except

---
+++ MangoTreeFinder.py
@@ -0,0 +1,16 @@
+# Creation of function which states whether tree is a Mango Tree or not
+def is_mango_tree(rows, columns, tree_number) :
+    if tree_number % columns == 1 or tree_number <= columns and tree_number > 0 or tree_number % columns == 0 :
+        return True
+    return False
+try :
+    rows = int(input("Enter the number of Rows : "))
+    columns = int(input("Enter the number of Columns : "))
+    tree_number = int(input("Enter the tree Number : "))
+
+    print(is_mango_tree(rows, columns, tree_number))
+
+except ValueError :                       # Accepts only valid inputs else handles exception
+    print("Invalid Input!!! Please Enter Integers Only that are greater than 0")
+
+
